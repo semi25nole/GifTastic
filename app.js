c91d48695f4dec13f81c3bb61779e8c1c9b23edd
Kyle Ruschel <dkr1985@icloud.com>
2017-09-01 02:26:37 -0400

    final

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 var giffy = ["Joe Dirt", "Wedding Crashers", "Remember the Titans", "Old School", "Masterminds", "Meet the Millers", "Caddyshack",
-    "Legends of Bagger Vance", "Good Will Hunting", "Fast Times at Ridgemont High"];
+    "Legends of Bagger Vance", "Good Will Hunting", "Fast Times at Ridgemont High"
+];
 
 var key = "5868968ce2344a4badf81372395500c1";
 
 //var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=5868968ce2344a4badf81372395500c1&q=' + giffy[x] + '&limit=10&rating=PG-13&lang=en';
 
-for(var x = 0; x < giffy.length; x++){
+for (var x = 0; x < giffy.length; x++) {
     console.log(giffy[x]);
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + giffy[x] + "&api_key=" + key + "&limit=10";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + giffy[x] + "&api_key=" + key + "&limit=10";
 }
 
-function buttons() {$("#btns").empty();
+function buttons() {
+    $("#btns").empty();
 
     for (var i = 0; i < giffy.length; i++) {
-    	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + giffy[i] + "&api_key=" + key + "&limit=10";
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + giffy[i] + "&api_key=" + key + "&limit=10";
         var a = $('<button class="btn btn-success btn-xs">');
 
         a.addClass("movie");
@@ -36,29 +38,49 @@ $('#search').on("click", function(event) {
     buttons();
 })
 
-$(document).on("click", '.movie', function() {
 
-    $('#gifs').empty();
+$(document).ready(function() {
 
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + $(this).attr('data-name') + "&api_key=" + key + "&limit=10";
+    $(document).on("click", '.movie', function() {
 
-    $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-        .done(function(response) {
-            console.log(queryURL);
-            console.log(response);
-            var results = response.data;
-            for (var l = 0; l < results.length; l++) {
-                var movieDiv = $('<div>');
-                var p = $('<p>').text("Rating: " + results[l].rating);
-                var movieImage = $('<img>');
-                movieImage.attr("src", results[l].images.fixed_height.url);
-                movieDiv.append(p);
-                movieDiv.append(movieImage);
-                $("#gifs").append(movieDiv);
+        $('#gifs').empty();
 
-            }
-        })
-})
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + $(this).attr('data-name') + "&api_key=" + key + "&limit=10";
+
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+            .done(function(response) {
+                console.log(queryURL);
+                console.log(response);
+
+                var results = response.data;
+                for (var l = 0; l < results.length; l++) {
+                    var movieDiv = $('<div>');
+                    var p = $('<p>').text("Rating: " + results[l].rating);
+                    var movieImage = $('<img>');
+                    movieImage.attr("src", results[l].images.fixed_height_still.url);
+                    movieImage.attr("data-still", results[l].images.fixed_height_still.url);
+                    movieImage.attr("data-animate", results[l].images.fixed_height.url);
+                    movieImage.attr("data-state", "still");
+                    movieImage.addClass("movie-image");
+                    movieDiv.append(p);
+                    movieDiv.append(movieImage);
+                    $("#gifs").append(movieDiv);
+                }
+
+            })
+    });
+
+    $(this).on("click", ".movie-image", function() {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    })
+});
